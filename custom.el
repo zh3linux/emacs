f69715931b2748d052f6c24e2bda6fdd0e96028a
zh3linux <zh3linux@gmail.com>
2013-11-04 18:49:27 +0800

    添加win number

--- custom.el
+++ custom.el
@@ -5,13 +5,16 @@
  ;; If there is more than one, they won't work right.
  '(ansi-color-faces-vector [default bold shadow italic underline bold bold-italic bold])
  '(ansi-color-names-vector (vector "#657b83" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#fdf6e3"))
- '(custom-enabled-themes (quote (tsdh-dark)))
- '(custom-safe-themes (quote ("4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" default)))
+ '(custom-enabled-themes (quote (sanityinc-tomorrow-eighties)))
+ '(custom-safe-themes (quote ("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" default)))
  '(fci-rule-color "#eee8d5")
  '(session-use-package t nil (session))
+ '(speedbar-show-unknown-files t)
  '(vc-annotate-background nil)
  '(vc-annotate-color-map (quote ((20 . "#dc322f") (40 . "#cb4b16") (60 . "#b58900") (80 . "#859900") (100 . "#2aa198") (120 . "#268bd2") (140 . "#d33682") (160 . "#6c71c4") (180 . "#dc322f") (200 . "#cb4b16") (220 . "#b58900") (240 . "#859900") (260 . "#2aa198") (280 . "#268bd2") (300 . "#d33682") (320 . "#6c71c4") (340 . "#dc322f") (360 . "#cb4b16"))))
- '(vc-annotate-very-old-color nil))
+ '(vc-annotate-very-old-color nil)
+ '(window-numbering-mode t)
+ '(winner-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -43,13 +46,13 @@
 (add-to-list 'load-path "weibo.emacs")
 (require 'weibo)
 ;;===========================================================================
+(require 'window-numbering)
+(window-numbering-mode 1)
 ;;sr-speedbar-mode
 ;;===========================================================================
 (require 'sr-speedbar)
 ;;默认显示所有文件
-(custom-set-variables
- '(speedbar-show-unknown-files t)
-)
+
 ;;sr-speedbar-right-side 把speedbar放在左侧位置
 ;;sr-speedbar-skip-other-window-p 多窗口切换时跳过speedbar窗口
 ;;sr-speedbar-max-width与sr-speedbar-width-x 设置宽度
@@ -59,3 +62,23 @@
 ;;(global-set-key (kbd "s-r") 'sr-speedbar-refresh-toggle)
 ;; 打开
 ;;(sr-speedbar-open)
+
+
+(autoload 'mew "mew" nil t)
+(autoload 'mew-send "mew" nil t)
+
+;; Optional setup (Read Mail menu for Emacs 21):
+(if (boundp 'read-mail-command)
+    (setq read-mail-command 'mew))
+
+;; Optional setup (e.g. C-xm for sending a message):
+(autoload 'mew-user-agent-compose "mew" nil t)
+(if (boundp 'mail-user-agent)
+    (setq mail-user-agent 'mew-user-agent))
+(if (fboundp 'define-mail-user-agent)
+    (define-mail-user-agent
+      'mew-user-agent
+      'mew-user-agent-compose
+      'mew-draft-send-message
+      'mew-draft-kill
+      'mew-send-hook))
---
+++ elpa/mew-20131026.1423/mew-pkg.el
@@ -0,0 +1,2 @@
+(define-package "mew" "20131026.1423" "Messaging in the Emacs World [github]" 'nil)
+
